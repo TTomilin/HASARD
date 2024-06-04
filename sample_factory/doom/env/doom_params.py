ab7ce96dfcb38a4595676fe59da0aa9db263a29d
TTomilin <tristan.tomilin@hotmail.com>
2024-06-04 08:22:50 +0200

    Longer and more frequent video recording

--- sample_factory/doom/env/doom_params.py
+++ sample_factory/doom/env/doom_params.py
@@ -38,8 +38,8 @@ def add_doom_env_args(parser):
     p.add_argument('--constraint', type=str, default='soft', choices=['soft', 'hard'], help='Soft/Hard safety constraint')
     p.add_argument('--render_mode', type=str, default='rgb_array', help='Rendering mode')
     p.add_argument("--video_dir", default='videos', type=str, help="Record episodes to this folder after an internval.")
-    p.add_argument("--video_length", default=1000, type=int, help="Length of recorded video.")
-    p.add_argument("--record_every", default=10000, type=int, help="Interval after how many steps to record a video.")
+    p.add_argument("--video_length", default=2500, type=int, help="Length of recorded video.")
+    p.add_argument("--record_every", default=1000, type=int, help="Interval after how many steps to record a video.")
     p.add_argument("--record", default=True, type=str2bool, help="Whether to record gameplay.")
 
 
